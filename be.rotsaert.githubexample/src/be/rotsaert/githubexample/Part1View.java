0e23b26d1501572dd57fd614539e7a0df2950159
dutchfreddie <frederik_rotsaert@hotmail.com>
2013-06-06 13:02:31 +0200

    Commited first time

---
+++ be.rotsaert.githubexample/src/be/rotsaert/githubexample/Part1View.java
@@ -0,0 +1,21 @@
+ 
+package be.rotsaert.githubexample;
+
+import javax.inject.Inject;
+import org.eclipse.e4.ui.di.Focus;
+
+public class Part1View {
+	@Inject
+	public Part1View() {
+		//TODO Your code here
+	}
+	
+	
+	
+	@Focus
+	public void onFocus() {
+		//TODO Your code here
+	}
+	
+	
+}
---
+++ be.rotsaert.githubexample/src/be/rotsaert/githubexample/Part2View.java
@@ -0,0 +1,21 @@
+ 
+package be.rotsaert.githubexample;
+
+import javax.inject.Inject;
+import org.eclipse.e4.ui.di.Focus;
+
+public class Part2View {
+	@Inject
+	public Part2View() {
+		//TODO Your code here
+	}
+	
+	
+	
+	@Focus
+	public void onFocus() {
+		//TODO Your code here
+	}
+	
+	
+}
